7cf10aecb1147c649691b8366f214c5f49623e9b
Dmitriy Lagutin <38376206+DmitryLagutin@users.noreply.github.com>
2019-12-24 15:54:10 +0300

    внесены правки 1009

--- main.py
+++ main.py
@@ -2,19 +2,29 @@ import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
+import json
+import datetime
 
-smtp_server = "p519990.mail.ihc.ru"
-port = 25  # For starttls
-sender_email = "aaa@lagunasun.ru"
+json_file = open("data.json", encoding='utf-8')
+data = json.loads(json_file.read())
+json_file.close()
+
+
+
+smtp_server = data["smtp_server"]
+port = data['port']
+sender_email = data['sender_email']
+password = data['password']
+
+timestamp = int(datetime.datetime.now().timestamp())
+
+
 receiver_email = 'dimalaga@mail.ru'
-password = 'go4no4123'
 
-message = ''
+message = 'Message {0}'.format(timestamp)
 
-# Create a secure SSL context
-context = ssl.create_default_context()
 
-messagePlain = 'Visit nitratine.net for some great tutorials and projects!'
+# messagePlain = 'Visit nitratine.net for some great tutorials and projects!'
 HTML = open('index.html', encoding="utf-8")
 messageHTML = HTML.read()
 HTML.close()
@@ -22,28 +32,20 @@ HTML.close()
 msg = MIMEMultipart('alternative')
 msg['From'] = sender_email
 msg['To'] = receiver_email
-msg['Subject'] = 'Супер предложние'
+msg['Subject'] = 'Супер предложение {0}'.format(timestamp+1)
 
-msg.attach(MIMEText(messagePlain, 'plain'))
-#with open('preview.jpg', 'rb') as fp:
-#    img = MIMEImage(fp.read())
-#    print(img)
-#msg.attach(img)
+# msg.attach(MIMEText(messagePlain, 'plain'))
 msg.attach(MIMEText(messageHTML, 'html'))
 
 text = msg.as_string()
 
-# Try to log in to server and send email
 try:
     server = smtplib.SMTP(smtp_server, port)
-    server.ehlo()  # Can be omitted
-    server.starttls(context=context)  # Secure the connection
-    server.ehlo()  # Can be omitted
+    server.ehlo()
     server.login(sender_email, password)
     server.sendmail(sender_email, receiver_email, text)
     # TODO: Send email here
 except Exception as e:
-    # Print any error messages to stdout
     print(e)
 finally:
     server.quit()
